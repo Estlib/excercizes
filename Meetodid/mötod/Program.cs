879e1453a8c09471b711265b980687e1e00a8317
Estlib <75094683+Estlib@users.noreply.github.com>
2026-01-22 13:19:03 +0200

    methods files

---
+++ Meetodid/mötod/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace mötod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MeetodMingisugune();
+            string pikkAsi = "aöeslkjrjgoipjesa4nmtgpojönw4eojklöthgnkjlkwjrhfgighweirughiuwe<hrgivuhaeiurhgiueahrioguhqaeouitrjhg oiuwetenhgpljweantrhlkj";
+            int loendatudTähed = TäheLugeja(pikkAsi);
+            Console.WriteLine(loendatudTähed);
+            int loendatudKindladTähed = TäheSorteerija(pikkAsi, "ö");
+            Console.WriteLine(loendatudKindladTähed);
+            //kirjuta meetod mis otsib kindlat elementi stringi järjendist
+        }
+
+        private static string MeetodMingisugune()
+        {
+            Console.WriteLine("AAAAAAAAAAAAAA");
+            return string.Empty;
+        }
+
+        public static int TäheLugeja(string sisendSõna)
+        {
+            return sisendSõna.Count();
+        }
+        public static int TäheSorteerija(string sisendSõna, string filter)
+        {
+            int kokku = 0;
+            foreach (var täht in sisendSõna)
+            {
+                if(täht.ToString() == filter)
+                {
+                    kokku++;
+                }
+            }
+
+            return kokku;
+        }
+    }
+}
